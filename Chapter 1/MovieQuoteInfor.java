578f87a59c63fb220494768cf2b151c9f6cfecec
berdiki <kien.berdida@gmail.com>
2024-08-14 19:01:08 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfor.java
@@ -0,0 +1,8 @@
+public class MovieQuoteInfor {
+    public static void main(String[] args) {
+        System.out.println("You are nothing but a second-rate, trying hard copycat!");
+        System.out.println(" ");
+        System.out.println("By: Cherie Gil");
+        System.out.println("Bituing Walang Ningning (1985)");
+    }
+}

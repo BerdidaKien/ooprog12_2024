85bdeb1422ef820834c5f3638a8d49c6d272fa0a
BERDIDA-KIEN <kien.berdida@gmail.com>
2024-09-20 09:33:52 +0800

    Create FirstAddressExercise.java

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,17 @@
+public class FirstAddressExercise {
+
+    public static void main(String[] args) {
+        String comPany = "Apple Inc.";
+        String address1 = "8900 U.S Hwy 14";
+        String address2 = "Crystal Lake, IL 60014";
+
+        displayCompanyAddress(comPany, address1, address2);
+    }
+
+    public static void displayCompanyAddress(String company, String address1, String address2) {
+        System.out.println("XYZ Company");
+        System.out.println(address1);
+        System.out.println(address2);
+        System.out.println("Company: " + company);
+    }
+}
